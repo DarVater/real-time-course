e0f21fd5bc32ddc9bfa361056afc7af953663168
bilcko <iubilcko@gmail.com>
2022-07-16 15:15:09 +0300

    event sourcing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import LongPulling from "./components/LongPulling";
+import EventSourcing from "./components/EventSourcing";
 
 function App() {
   return (
     <div className="app">
-      <LongPulling/>
+      <EventSourcing/>
     </div>
   );
 }
---
+++ src/components/EventSourcing.jsx
@@ -0,0 +1,52 @@
+import React, {useEffect, useState} from 'react';
+import axios from "axios";
+import '../App.css'
+
+const LongPulling = () => {
+    const [messages, setMessages] = useState([])
+    const [value, setValue] = useState('')
+
+    useEffect(() => {
+        subscribe()
+    }, [])
+
+    const subscribe = async () => {
+        const eventSource = new EventSource('http://localhost:5000/connect')
+        eventSource.onmessage = function (event) {
+            const message = JSON.parse(event.data)
+            setMessages(prev => [message, ...prev])
+        }
+    }
+
+    const sendMessage =  () => {
+        console.log(value)
+        axios.post('http://localhost:5000/new-messages', {
+            id: Date.now(),
+            message: value,
+        })
+    }
+
+    return (
+        <div className="center">
+            <div className="form">
+                <input
+                    onChange={e => setValue(e.target.value)}
+                    value={value}
+                    type="text"
+                />
+                <button onClick={sendMessage}>Отправить</button>
+            </div>
+            <div className="messages">
+                {messages && messages.map(mess =>
+                    mess == undefined ? '':
+                    <div key={mess.id} className="message" >
+                        {mess.message}
+                    </div>
+                )}
+            </div>
+            
+        </div>
+    );
+};
+
+export default LongPulling;
